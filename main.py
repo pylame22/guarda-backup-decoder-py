88ba1d8889810a957f9a17e84fa5bb060fdca6f5
pylame22 <pylame22@yandex.ru>
2024-02-18 03:58:47 +0300

    update ruff version

--- main.py
+++ main.py
@@ -3,26 +3,31 @@ import logging
 from base64 import b64decode
 from getpass import getpass
 from hashlib import md5, pbkdf2_hmac
+from pathlib import Path
 
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 
 
 def _get_backup() -> str:
-    with open("guarda_backup.txt") as file:
+    with Path.open("guarda_backup.txt") as file:
         return file.read()
 
 
+class BackupDecryptError(Exception):
+    pass
+
+
 class GuardaBackupDecrypt:
-    _PASSWORD_SALT = "XB7sHH26Hn&FmPLxnjGccKTfPV(yk"
-    _PASSWORD_POSTFIX = "(tXntTbJFzh]4EuQVmjzM9GXHCth8"
+    _PASSWORD_SALT = "XB7sHH26Hn&FmPLxnjGccKTfPV(yk"  # noqa: S105
+    _PASSWORD_POSTFIX = "(tXntTbJFzh]4EuQVmjzM9GXHCth8"  # noqa: S105
     _BLOCK_SIZE = 16
 
     @staticmethod
     def _get_final_key(data: bytes, *, output_count: int) -> bytes:
         key, final_key = b"", b""
         while len(final_key) < output_count:
-            key = md5(key + data).digest()
+            key = md5(key + data).digest()  # noqa: S324
             final_key += key
         return final_key[:output_count]
 
@@ -36,14 +41,14 @@ class GuardaBackupDecrypt:
         self._backup_encrypted = b64decode(backup_str)
 
     def _get_key_nonce(self) -> tuple[bytes, bytes]:
-        data = self._password + self._backup_encrypted[self._BLOCK_SIZE // 2: self._BLOCK_SIZE]
+        data = self._password + self._backup_encrypted[self._BLOCK_SIZE // 2 : self._BLOCK_SIZE]
         final_key = self._get_final_key(data, output_count=self._BLOCK_SIZE * 3)
-        return final_key[:self._BLOCK_SIZE * 2], final_key[self._BLOCK_SIZE * 2:]
+        return final_key[: self._BLOCK_SIZE * 2], final_key[self._BLOCK_SIZE * 2 :]
 
     def decrypt(self) -> str:
         key, nonce = self._get_key_nonce()
         aes = AES.new(key, AES.MODE_CBC, nonce)
-        cipher = aes.decrypt(self._backup_encrypted[self._BLOCK_SIZE:])
+        cipher = aes.decrypt(self._backup_encrypted[self._BLOCK_SIZE :])
         return unpad(cipher, self._BLOCK_SIZE).decode()
 
 
@@ -54,7 +59,8 @@ def main() -> None:
     try:
         data = gbd.decrypt()
     except ValueError:
-        raise Exception("Wrong password") from None
+        message = "Wrong password"
+        raise BackupDecryptError(message) from None
     data_json = json.loads(data)
     logging.info(json.dumps(data_json, indent=2))
 
